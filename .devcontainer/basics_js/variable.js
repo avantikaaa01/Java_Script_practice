bb9da29a5cf39593452e7cfb22cb328c44b82402
avantikaaa01 <164382895+avantikaaa01@users.noreply.github.com>
2025-10-31 20:08:14 +0000

    completed variable topic

---
+++ .devcontainer/basics_js/variable.js
@@ -0,0 +1,11 @@
+// Basic Information
+let name = "Avantika"
+let divison = 12
+const rollNo = 2398
+state = "Banglore"
+
+// console.log(name);
+// console.log(divison);
+
+// console.table([name, divison, rollNo,state])
+console.table([name, state])
