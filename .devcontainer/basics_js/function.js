f98c6fd8e08bfe530c6dd5d49a9c5773ae39a65f
avantikaaa01 <164382895+avantikaaa01@users.noreply.github.com>
2025-11-02 14:40:10 +0000

    add Functions

---
+++ .devcontainer/basics_js/function.js
@@ -0,0 +1,53 @@
+
+// Write a function called sayHello() that prints "Hello, JavaScript!" to the console.
+// function sayHello(){
+//     console.log("hello, javascript");
+// }
+
+// sayHello();
+
+// write a function greetUser(name) that takes a name as an argument and prints "Hello, [name]!".
+
+// function greetUser(name){
+//     console.log("hello," + name + "!");
+// }
+// greetUser("avantika");
+
+// Write a function addNumbers(a, b) that returns the sum of two numbers.
+
+// function addNumbers(a,b){
+//     return a + b;
+// }
+// result = addNumbers(5,9);
+// console.log(result);
+
+// Write a function factorial(n) that returns the factorial of a number.
+// function factorial(n){
+//     if (n == 0 || n ==1 )
+//         return 1;
+//     return n * factorial(n - 1);
+// }
+
+// result = factorial(5);
+// console.log(result);
+
+// Write a function square(num) that returns the square of a number.
+
+// function square(num){
+//     return num * num;
+// }
+
+// let result = square(99);
+// console.log(result);
+
+// Write a function isEven(num) that returns true if a number is even, otherwise false.
+
+// function isEven(num){
+//     if(num % 2 == 0)
+//         return true;
+//     if(num % 2 != 0)
+//         return false;
+// }
+
+// let result = isEven(29);
+// console.log(result);
---
+++ .devcontainer/basics_js/function2.js
@@ -0,0 +1,28 @@
+// Write a function findMax(a, b, c) that returns the largest of three numbers.
+
+// function findMax(a,b,c){
+//     if(a>b && a >c){
+//         return a ;
+//     } 
+//     else if(b > a && b > c){
+//         return b;
+//     }
+//     else if(c > a && c > b){
+//         return c;
+//     } 
+// }
+// let result = findMax(90, 6, 9);
+// console.log(result);
+
+// Write a function printNumbers(n) that prints numbers from 1 to n.
+
+// function printNumbers(n){
+//     for(let i = 1; i <=n ; i++){
+//         console.log(i);
+
+//     }
+// }
+// printNumbers(23);
+
+// Write a function sumTillN(n) that returns the sum of all numbers from 1 to n.
+
