d87a150ba98f70b87ec3742492d2192213bd4e60
avantikaaa01 <164382895+avantikaaa01@users.noreply.github.com>
2025-11-03 13:07:47 +0000

    added function2

--- .devcontainer/basics_js/function2.js
+++ .devcontainer/basics_js/function2.js
@@ -26,3 +26,28 @@
 
 // Write a function sumTillN(n) that returns the sum of all numbers from 1 to n.
 
+// function sumTill(n){
+//     let sum = 0;
+//     for(let i = 1; i <=n; i++){
+//         sum = sum + i;
+//     }
+//     return sum;
+    
+// }
+// let result = sumTill(2);
+// console.log(result);
+
+
+// Write a function reverseNumber(num) that reverses a number.
+
+function reverseNumber(num){
+    let rev = 0;
+    while(num > 0){
+        let digit = num % 10;
+        rev = rev * 10 + digit;
+        num = Math.floor(num / 10);
+    }
+    return rev;
+}
+console.log(reverseNumber(1234));
+
